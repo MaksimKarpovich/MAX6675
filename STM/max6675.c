35cf62bf6dc6182de55ef1f90d00b60e79cdbd5d
Maksim Karpovich <maxim.karpovich.2000@gmail.com>
2023-02-24 15:26:46 +0300

    feat: upgrade config file

--- STM/max6675.c
+++ STM/max6675.c
@@ -13,7 +13,7 @@ float max6675_readTemperature(void)
     uint8_t rxData[ARRAY_SIZE];
     float Temperature = 0;
     SPI_CS_SET_LOW();
-    HAL_SPI_Receive(&hspi1, rxData, ARRAY_SIZE, SPI_WAIT_TIME);
+    HAL_SPI_Receive(&USING_SPI, rxData, ARRAY_SIZE, SPI_WAIT_TIME);
     SPI_CS_SET_HIGH();
     Temperature = (float) (rxData[1] | (rxData[0] << BYTE_SIZE)) >> (REGISTER_OFFSET + DEGREES_OFFSET);               // Temperature Data Extraction
 //    HAL_Delay(250);                                       // Waits for Chip Ready(according to Datasheet, the max time for conversion is 220ms)
--- STM/max6675_config.h
+++ STM/max6675_config.h
@@ -4,6 +4,8 @@
 
 #define SPI1_CS_GPIO_Port SPI1_CS_GPIO_Port
 #define SPI1_CS_Pin SPI1_CS_Pin
+#define USING_SPI hspi1
+
 #define SPI_CS_SET_HIGH()   HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin, GPIO_PIN_SET)
 #define SPI_CS_SET_LOW()   HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin, GPIO_PIN_RESET)
 
